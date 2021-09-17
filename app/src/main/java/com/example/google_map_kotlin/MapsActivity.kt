2f2a96bdce4c1239f9293d5db718b6ae2ac1201e
Mubarak <mubarakansari715@gmail.com>
2021-09-17 18:40:24 +0530

    Initial

---
+++ app/src/main/java/com/example/google_map_kotlin/MapsActivity.kt
@@ -0,0 +1,62 @@
+package com.example.google_map_kotlin
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.google_map_kotlin.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(21.534630, 71.827484)
+        map.addMarker(MarkerOptions().position(sydney).title("My Location"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        //currentLocation
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        map.isMyLocationEnabled = true
+
+        //zoomButton
+        map.uiSettings.isZoomControlsEnabled = true
+
+        map.uiSettings.isCompassEnabled = true
+        map.uiSettings.isRotateGesturesEnabled = true
+
+
+    }
+}
