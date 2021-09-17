b4e0da498cd23a5747945ac884e3acc069b39f06
Mubarak <mubarakansari715@gmail.com>
2021-09-17 22:51:12 +0530

    Initial

--- app/src/main/java/com/example/google_map_kotlin/MapsActivity.kt
+++ app/src/main/java/com/example/google_map_kotlin/MapsActivity.kt
@@ -2,7 +2,10 @@ package com.example.google_map_kotlin
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.google_map_kotlin.databinding.ActivityMapsBinding
@@ -12,11 +15,18 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity() {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,39 +34,108 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            Log.d("GoogleMap", "before isMyLocationEnabled")
+//            googleMap.isMyLocationEnabled = true
+            val location1 = LatLng(13.0356745,77.5881522)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+
+            Log.d("GoogleMap", "before location3")
+
+            val location3 = LatLng(13.029727,77.5933021)
+            googleMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+
+            Log.d("GoogleMap", "before URL")
+            val URL = getDirectionURL(location1,location3)
+            Log.d("GoogleMap", "URL : $URL")
+            GetDirection(URL).execute()
+
+        })
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
+    fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving"
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(21.534630, 71.827484)
-        map.addMarker(MarkerOptions().position(sydney).title("My Location"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
-        //currentLocation
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
+    private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body()!!.string()
+            Log.d("GoogleMap" , " data : $data")
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,GoogleMapDTO::class.java)
+
+                val path =  ArrayList<LatLng>()
+
+                for (i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
+//                    path.add(startLatLng)
+//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
         }
-        map.isMyLocationEnabled = true
 
-        //zoomButton
-        map.uiSettings.isZoomControlsEnabled = true
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+            googleMap.addPolyline(lineoption)
+        }
+    }
 
-        map.uiSettings.isCompassEnabled = true
-        map.uiSettings.isRotateGesturesEnabled = true
+    public fun decodePolyline(encoded: String): List<LatLng> {
 
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
 
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
     }
 }
